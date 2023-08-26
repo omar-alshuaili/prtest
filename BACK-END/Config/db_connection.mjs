5a4aecc5142d8e3e09264508ed88a439e74358fc
OmarShuaili <omar.shuaili@hotmail.com>
2023-08-26 19:51:24 +0100

    database

--- BACK-END/Config/db_connection.mjs
+++ BACK-END/Config/db_connection.mjs
@@ -7,6 +7,6 @@ dotenv.config();
 // CONNECTING SERVER WITH DATABASE
 mongoose.set("strictQuery", false);
 
-mongoose.connect(process.env.DB_LOCAL_STRING)
+mongoose.connect(process.env.DB_LOCAL_STRING || "mongodb+srv://omar:5OKA4ZE7ylh5fbEm@cluster0.u8au1eu.mongodb.net/")
   .then(() => console.log("Database is attached."))
   .catch((error) => console.log(error))
